60da1bd7f109581f798eb9059b51be72b9fa5416
Wendell Sun <iwendellsun@gmail.com>
2022-02-05 22:17:13 +0800

    add github workflow

--- cmd/freeproxy/main.go
+++ cmd/freeproxy/main.go
@@ -14,6 +14,7 @@ import (
 func main() {
 	homeDir, _ := os.UserHomeDir()
 	app := &cli.App{
+		Usage: "A command-line free proxy manager",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "config",
